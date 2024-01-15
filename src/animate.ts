26023b85e655384a0498e683b2fb24a405853801
onsclom <onsclom@onsclom.net>
2024-01-15 13:10:39 -0800

    Clamp animation distance travelled

--- src/animate.ts
+++ src/animate.ts
@@ -1,56 +1,85 @@
-import { State } from "./main"
-import { ease, lerp, vec2Equals } from "./utils"
+import { State } from "./main";
+import { ease, lerp, vec2Equals } from "./utils";
 
 export function animate(state: State, delta: number) {
-  const { charHeight: CHAR_HEIGHT, charWidth: CHAR_WIDTH } = state.sizes
+  const { charHeight: CHAR_HEIGHT, charWidth: CHAR_WIDTH } = state.sizes;
   // TODO: make more generic way to add animations
   // maybe just timers for example?
   state.text.forEach((row) => {
     row.forEach((letter) => {
-      letter.time += delta / 1000
-      letter.time = Math.min(state.settings.letterAnimationTime, letter.time)
+      letter.time += delta / 1000;
+      letter.time = Math.min(state.settings.letterAnimationTime, letter.time);
 
-      letter.translateTime += delta / 1000
+      letter.translateTime += delta / 1000;
       letter.translateTime = Math.min(
         state.settings.letterAnimationTime,
         letter.translateTime
-      )
+      );
+
+      const dist = Math.sqrt(
+        (letter.visualStart.x - letter.pos.x) ** 2 +
+          (letter.visualStart.y - letter.pos.y) ** 2
+      );
+      const MAX_DIST = 5;
+      if (dist > MAX_DIST) {
+        letter.visualStart = {
+          x: letter.pos.x + (letter.visualStart.x - letter.pos.x) / dist,
+          y: letter.pos.y + (letter.visualStart.y - letter.pos.y) / dist,
+        };
+      }
+
       letter.visualCur = lerp(
         letter.visualStart,
         letter.pos,
         ease(letter.translateTime / state.settings.letterAnimationTime)
-      )
-    })
-  })
+      );
+    });
+  });
 
   // animate cursor
   if (!vec2Equals(state.cursor.lastPos, state.cursor.pos)) {
+    const dist = Math.sqrt(
+      (state.cursor.visualStart.x - state.cursor.pos.x) ** 2 +
+        (state.cursor.visualStart.y - state.cursor.pos.y) ** 2
+    );
+    const MAX_DIST = 5;
+    if (dist > MAX_DIST) {
+      state.cursor.visualStart = {
+        x:
+          state.cursor.pos.x +
+          (state.cursor.visualStart.x - state.cursor.pos.x) / dist,
+        y:
+          state.cursor.pos.y +
+          (state.cursor.visualStart.y - state.cursor.pos.y) / dist,
+      };
+    }
+
     // cursor moved so start animation timer
     state.cursor.visualTarget = {
       x: state.cursor.pos.x * CHAR_WIDTH,
       y: state.cursor.pos.y * CHAR_HEIGHT,
-    }
-    state.cursor.visualStart = state.cursor.visualPos
-    state.cursor.visualTime = 0
-    state.cursor.lastPos = structuredClone(state.cursor.pos)
+    };
+    state.cursor.visualStart = state.cursor.visualPos;
+    state.cursor.visualTime = 0;
+    state.cursor.lastPos = structuredClone(state.cursor.pos);
   }
-  state.cursor.visualTime += delta / 1000
+  state.cursor.visualTime += delta / 1000;
   state.cursor.visualTime = Math.min(
     state.settings.cursorAnimationTime,
     state.cursor.visualTime
-  )
+  );
   state.cursor.visualPos = lerp(
     state.cursor.visualStart,
     state.cursor.visualTarget,
     ease(state.cursor.visualTime / state.settings.cursorAnimationTime)
-  )
+  );
 
   state.letterGraveyard.forEach((letter) => {
-    letter.time += delta / 1000
-    letter.time = Math.min(state.settings.letterAnimationTime, letter.time)
-  })
+    letter.time += delta / 1000;
+    letter.time = Math.min(state.settings.letterAnimationTime, letter.time);
+  });
   // remove letters that are too old
   state.letterGraveyard = state.letterGraveyard.filter(
     (letter) => letter.time < state.settings.letterAnimationTime
-  )
+  );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,55 +1,55 @@
-import { state } from "./main"
+import { state } from "./main";
 
 export function lerp(pos1: Vec2, pos2: Vec2, t: number) {
   return {
     x: pos1.x + (pos2.x - pos1.x) * t,
     y: pos1.y + (pos2.y - pos1.y) * t,
-  }
+  };
 }
 
 export function ease(x: number): number {
-  return 1 - Math.pow(1 - x, 3)
+  return 1 - Math.pow(1 - x, 4);
 }
 
 export function clamp(min: number, max: number, value: number) {
-  return Math.max(min, Math.min(max, value))
+  return Math.max(min, Math.min(max, value));
 }
 
 export function vec2Equals(pos1: Vec2, pos2: Vec2) {
-  return pos1.x === pos2.x && pos1.y === pos2.y
+  return pos1.x === pos2.x && pos1.y === pos2.y;
 }
 
 export function wordWrap() {
-  const charsPerRow = state.sizes.charsPerRow
-  let line = 0
-  let row = 0
-  let lastCharWasNewline = false
+  const charsPerRow = state.sizes.charsPerRow;
+  let line = 0;
+  let row = 0;
+  let lastCharWasNewline = false;
   const cursorAttachedTo = {
     x: state.cursor.pos.x - 1,
     y: state.cursor.pos.y,
-  }
+  };
 
-  const textChars = state.text.flat()
-  const newText = [] as typeof state.text
+  const textChars = state.text.flat();
+  const newText = [] as typeof state.text;
 
   textChars.forEach((textChar) => {
     if (lastCharWasNewline) {
-      lastCharWasNewline = false
-      row = 0
-      line++
+      lastCharWasNewline = false;
+      row = 0;
+      line++;
     }
 
     if (textChar.letter === "\n") {
-      lastCharWasNewline = true
+      lastCharWasNewline = true;
     } else if (row >= charsPerRow) {
-      row = 0
-      line++
+      row = 0;
+      line++;
     }
 
     const newPos = {
       x: row,
       y: line,
-    }
+    };
 
     if (!vec2Equals(textChar.pos, newPos)) {
       // if cursor right after this char, move it to the new position too
@@ -59,17 +59,17 @@ export function wordWrap() {
           state.cursor.pos = {
             x: newPos.x + 1,
             y: newPos.y,
-          }
+          };
         }
       }
-      textChar.visualStart = textChar.visualCur
-      textChar.pos = newPos
-      textChar.translateTime = 0
+      textChar.visualStart = textChar.visualCur;
+      textChar.pos = newPos;
+      textChar.translateTime = 0;
     }
-    newText[line] = newText[line] || []
-    newText[line][row] = textChar
+    newText[line] = newText[line] || [];
+    newText[line][row] = textChar;
 
-    row++
-  })
-  state.text = newText
+    row++;
+  });
+  state.text = newText;
 }
